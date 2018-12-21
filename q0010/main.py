7e3dd6d806ce86ca9dd6625bf17dca727531c995
law52525 <law52526@qq.com>
2018-12-21 14:52:08 +0800

    fix

--- q0010/main.py
+++ q0010/main.py
@@ -14,15 +14,16 @@ def random_color2():
     return randint(32, 127), randint(32, 127), randint(32, 127)
 
 
-height = 60
-width = height * 4
-image = Image.new('RGB', (width, height), (255, 255, 255))
-font = ImageFont.truetype('Arial.ttf', 36)
-draw = ImageDraw.Draw(image)
-for x in range(width):
-    for y in range(height):
-        draw.point((x, y), fill=random_color())
-for t in range(4):
-    draw.text((height * t + 10, 10), random_char(), font=font, fill=random_color2())
-# image = image.filter(ImageFilter.BLUR)  # 模糊
-image.show()
+if __name__ == '__main__':
+    height = 60
+    width = height * 4
+    image = Image.new('RGB', (width, height), (255, 255, 255))
+    font = ImageFont.truetype('Arial.ttf', 36)
+    draw = ImageDraw.Draw(image)
+    for x in range(width):
+        for y in range(height):
+            draw.point((x, y), fill=random_color())
+    for t in range(4):
+        draw.text((height * t + 10, 10), random_char(), font=font, fill=random_color2())
+    image = image.filter(ImageFilter.BLUR)  # 模糊
+    image.show()
