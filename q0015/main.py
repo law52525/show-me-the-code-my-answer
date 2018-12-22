24d057df85e3ba0105663e25b1986e7eb49b7d65
law52525 <law52526@qq.com>
2018-12-22 13:32:37 +0800

    fix

--- q0015/main.py
+++ q0015/main.py
@@ -4,10 +4,10 @@ import xlwt
 if __name__ == '__main__':
     with open('city.txt', encoding='utf-8') as f:
         s = f.read()
-    students = json.loads(s)
+    city = json.loads(s)
     book = xlwt.Workbook()
     sheet = book.add_sheet('city')
-    for i, k in enumerate(students.keys()):
+    for i, k in enumerate(city.keys()):
         sheet.write(i, 0, k)
-        sheet.write(i, 1, students[k])
+        sheet.write(i, 1, city[k])
     book.save('city.xls')
