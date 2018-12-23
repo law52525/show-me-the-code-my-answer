409e06e4e737b6ae751023d95656415f2ad1a551
law52525 <law52526@qq.com>
2018-12-23 12:10:42 +0800

    fix

--- q0004/main.py
+++ q0004/main.py
@@ -1,11 +1,17 @@
 import re
 
-if __name__ == '__main__':
-    with open('data.txt', encoding='utf-8') as f:
+
+def main(filename):
+    with open(filename, encoding='utf-8') as f:
         s = f.read()
     results = list(filter(None, re.split(r'["-:?!.;,\s]+', s)))
     a = {r: 0 for r in results}
     for r in results:
         a[r] += 1
+    return a
+
+
+if __name__ == '__main__':
+    a = main('data.txt')
     for v in sorted(a.items(), key=lambda x: x[1], reverse=True):
         print("{}: {}".format(v[0], v[1]))
