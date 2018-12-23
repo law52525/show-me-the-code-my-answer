b8adf90f24b20508ddd62ec0fa0e3d36b6b4256b
law52525 <law52526@qq.com>
2018-12-23 13:00:45 +0800

    answer 0006

---
+++ q0006/main.py
@@ -0,0 +1,22 @@
+import os
+from q0004.main import main
+
+# http://www.newxue.com/mingzhu/anniriji/
+diary_path = "Het Achterhuis"
+
+
+def filter_words(t):
+    s = t[0].upper()
+    j = []
+    with open("filter_words.txt", encoding='utf-8') as f:
+        j.append(f.read().upper().split('\n'))
+    j.append(chr(v) for v in range(ord('A'), ord('Z') + 1))
+    judge = []
+    for i in j:
+        judge.append(s not in i)
+    return all(judge)
+
+
+if __name__ == '__main__':
+    for file in os.listdir(diary_path):
+        print(*filter(filter_words, main(diary_path + os.sep + file)).__next__())
