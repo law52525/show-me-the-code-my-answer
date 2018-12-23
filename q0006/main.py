16e63cd4aa01219809157ef3d91b2ea0f5bd8cc3
law52525 <law52526@qq.com>
2018-12-23 16:41:09 +0800

    fix

--- q0006/main.py
+++ q0006/main.py
@@ -7,14 +7,13 @@ diary_path = "Het Achterhuis"
 
 def filter_words(t):
     s = t[0].upper()
-    j = []
+    j = list()
     with open("filter_words.txt", encoding='utf-8') as f:
         j.append(f.read().upper().split('\n'))
     j.append(chr(v) for v in range(ord('A'), ord('Z') + 1))
-    judge = []
-    for i in j:
-        judge.append(s not in i)
-    return all(judge)
+    for i in range(len(j)):
+        j[i] = s not in j[i]
+    return all(j)
 
 
 if __name__ == '__main__':
